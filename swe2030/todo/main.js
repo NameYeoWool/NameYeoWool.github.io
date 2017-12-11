eed02624b40df8b08193f13a3f5be35fdfbbc1b2
YeoWool <gkaudrl542@daum.net>
2017-12-11 23:41:23 +0900

    fix

--- swe2030/todo/main.js
+++ swe2030/todo/main.js
@@ -44,7 +44,7 @@ function network(command, end, arg){
             break;
         
         case "toggleComplete":
-            $.get(URLS.prefix + URLS.toggleComplete1,{id:arg}, end);
+            $.get(URLS.prefix + URLS.toggleComplete1,{id:arg[0],complete:arg[1]}, end);
             break;
             
         default: throw "invalid command";
