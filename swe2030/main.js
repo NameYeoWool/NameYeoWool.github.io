486c75333dc6c7565af375f1728b24ae84a05567
YeoWool <gkaudrl542@daum.net>
2017-12-11 20:56:13 +0900

    교수님 refactoring

---
+++ swe2030/main.js
@@ -0,0 +1,28 @@
+var URLS = {
+    prefix:
+    list:
+    eachList:
+    groupAdd:
+};
+
+data = {
+    data:[{},{},{}]
+}
+function network(command, end, arg){
+    switch(commnad){
+
+    case"addList":
+    $.get(URLS.)
+
+
+
+    // data.data.push(name)
+    // end
+
+    case"list":
+
+    //아래와 같이 end 만 넣어 주어도 서버와 통신하는 것 같이 돌아간다.
+    //#.get(URLS.prefix + URLS.list, end);
+    end{data[...]}
+    }
+}
---
+++ swe2030/todo/main.js
@@ -0,0 +1,49 @@
+var URLS = {
+    //prefix:"localhost:8080", //"http://kdy0962.github.io/todo/",
+    prefix:"http://NameYeoWool.github.io/swe2030/todo/",
+    icon:{
+        unchecked:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRYR4sh0fM1Buy__0F9ea9VWazxLdfj9fV0dGCse3mMKiW5UVR-Yw",
+        checked:"https://upload.wikimedia.org/wikipedia/commons/thumb/7/71/Ok_sign_font_awesome.svg/2000px-Ok_sign_font_awesome.svg.png"
+    },
+
+    list:"json/groupList.json",
+    eachList:"list.html",
+    groupAdd:"json/groupAdd.json",
+    taskList:"json/taskList.json",
+    taskAdd:"json/taskAdd.json"
+};
+
+
+
+function network(command, end, arg){
+    switch(command){
+        case "eachList":
+            //location.href="URI" 자동으로 특정페이지로 이동
+            location.href = URLS.prefix + URLS.eachList +"?name=" + encodeURIComponent(arg);
+            break;
+        case "addList":
+            $.get(URLS.prefix + URLS.groupAdd, {name:arg}, end);
+            /*
+            data.data.push({name:args..});
+            end(data);
+            */
+            break;
+        case "taskList":
+            $.get(URLS.prefix+ URLS.taskList, end);
+            break;
+        case "taskAdd":
+            $.get(URLS.prefix + URLS.taskAdd,{name:arg},end );
+            break;
+        case "list":
+            $.get(URLS.prefix + URLS.list, end);
+            /*
+            end(data);
+            */
+            break;
+        
+        case "toggleComplete":
+            $.get(URLS.prefix + URLS.taskList,{id:arg[0],complete:arg[1]}, end);
+        
+        default: throw "invalid command";
+    }
+}
